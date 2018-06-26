94a2d291514a383bf1d4fa9dd4035cb3ffe3ef41
WangGaoHui <2269879645@qq.com>
2018-06-26 09:26:59 +0800

    加载创建数据库和表格各项配置

---
+++ app/src/main/java/com/example/acer/coolweather/Province.java
@@ -0,0 +1,33 @@
+package com.example.acer.coolweather;
+
+import org.litepal.crud.DataSupport;
+//省级字段
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
